f0b6b2cda799a2dc5eca184c09cd854f2cefe2ff
Gunnar Kleiven <Gunnar.Kleiven@student.uib.no>
2022-05-10 21:32:06 +0200

    Finished package delivery mission example. Warning: Disabled heartbeats RabbitMQ!

--- backend/controllers/message_subscriber.py
+++ backend/controllers/message_subscriber.py
@@ -9,7 +9,12 @@ class MessageSubscriber:
         self.exchange_type = exchange_type
 
     def subscription(self):
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        # NOTE! heartbeat interval is disabled. This is bad practice, but is done so RabbitMQ does not cancel the 
+        # connection. 
+        # See GitHub issue: 
+        # https://github.com/pika/pika/issues/1224
+        # Disabled by doing: https://github.com/pika/pika/blob/1.0.1/pika/connection.py#L554
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', heartbeat=0))
         channel = connection.channel()
         channel.exchange_declare(exchange=self.exchange, exchange_type=self.exchange_type)
 
