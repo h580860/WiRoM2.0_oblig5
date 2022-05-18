c2eb6b571a45c770e3225d4ff086acd344e07dae
Gunnar <Gunnar.Kleiven@student.uib.no>
2022-05-18 12:56:36 +0200

    restructured task allocation algorithms into superclasses of centralized and decentralized algorithms

---
+++ backend/task_allocation/decentralized_taskallocation.py
@@ -0,0 +1,7 @@
+class DecentralicedTaskAllocation(TaskAllocationSuperclass):
+    """
+    In this class we can add more methods related to centralized allocation algorithms
+    """
+
+    def __init__(self):
+        pass
